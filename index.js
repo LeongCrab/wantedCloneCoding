8bb8f0e8756545e51d53abd506b87ed8ba652824
LeongCrab(렁게) <111481882+LeongCrab@users.noreply.github.com>
2022-10-10 15:02:32 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function menu() {
+    alert("메뉴");
+}
+
+function search() {
+    alert("검색 아이콘");
+}
+
+function login() {
+    alert("회원가입/로그인 버튼");
+}
+
+function career() {
+    alert(`취직/이직 준비하시기도 바쁘시죠? 😎 커리어 인사이트, 이제 따로 찾지 말고 원티드에서 만나보세요! 검증된 IT업계 전문가들이 다양한 채널에서 생산하는 커리어 콘텐츠를 선별해서 관심 태그 기반으로 제공해 드립니다.`);
+}
+
+function careerMore() {
+    alert("더 많은 콘텐츠 보기 클릭");
+}
